dee454b855eedbb96fadf118ab3a4c705442fbf3
coderaj2001 <biswajit05101963@gmail.com>
2022-02-14 12:07:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Board from "./components/Board";
+
+const App =() => {
+  return (
+
+    <>
+    <h1>TiC TAC TOE</h1>
+    <Board/>
+  </>
+    );
+  }
+  export default App;
+
+
---
+++ src/components/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = ({value}) => {    //Here we are destructing the object
+  return (
+    
+     <button type="button">{value}</button>
+    
+  )
+}
+
+export default Square;
