2b87be3a1680ce0e4e0d419fa8c80e960407d31a
coderaj2001 <biswajit05101963@gmail.com>
2022-02-14 21:09:49 +0530

    added Draw message heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import Board from "./components/Board";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 import { calculateWinner } from "./components/helpers";
 import "./Style/root.scss";
 
@@ -61,7 +62,7 @@ const message= winner ? `winner is ${winner}`: `Next player is ${current.isXNext
 
     <div className="app">
     <h1>TiC TAC TOE</h1>
-    <h2>{message}</h2>
+    <StatusMessage winner={winner} current={current} />
     <Board board={current.board} handleSquareClick={handleSquareClick}/>
     <History history={history} moveTo={moveTo} currentMove={currentmove} />
   </div>
